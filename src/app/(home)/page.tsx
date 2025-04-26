9c02fe7fb68d61bba37c2844a00d4ed8f12c8a72
Davi GV <al.davigvalerio@gmail.com>
2025-04-26 14:42:16 -0300

    feat(home): add banner

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,22 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
 
 import { SearchInput } from '@/app/(home)/(components)/search-input';
 import { Header } from '@/components/header/header';
 
 const HomePage: NextPage = () => (
-  <div className="flex flex-col gap-2">
-    <div className="flex flex-col bg-primary">
+  <div className="flex flex-col">
+    <div className="flex flex-col bg-primary mb-[1px]">
       <Header className="pb-0" />
       <SearchInput />
     </div>
-    <h1>Hello</h1>
+    <Image
+      priority
+      width={390}
+      height={130}
+      src="/banner.png"
+      alt="Rango barato no dia das crianças! Peça com até 50% OFF"
+    />
   </div>
 );
 
