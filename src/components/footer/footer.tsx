917f787738ea7ac7926728322b7c83ac0a606c19
Davi GV <al.davigvalerio@gmail.com>
2025-04-28 22:38:52 -0300

    refactor(footer): set correct text sizes

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 
 export const Footer: FC = () => (
   <footer className="text-primary flex flex-col gap-2 bg-neutral-100 px-4 py-6 text-center font-bold">
-    <span>feito com 💜 em maringá-PR</span>
-    <span>
+    <span className="text-sm">feito com 💜 em maringá-PR</span>
+    <span className="leading-[22px]">
       aiqfome.com © 2007-2023 aiqfome LTDA .<br /> CNPJ: 09.186.786/0001-58
     </span>
   </footer>
